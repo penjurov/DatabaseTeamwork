04c791ab849ab499c14a9be3bd3892490403c2d9
neutrino <jrusev@gmail.com>
2014-08-29 16:16:43 +0300

    changed xml output schema

--- Clinics/Clinics/Exports/ExportToXML.cs
+++ Clinics/Clinics/Exports/ExportToXML.cs
@@ -19,9 +19,12 @@
             int month = this.Month.SelectedIndex + 1;
             int year = int.Parse(this.Year.Text);
 
-            // Generating monthly report by clinic as XML file...
-            var reports = RetrieveReportFromDB(year, month);
-            var clinicsReport = new XElement("expenses", GetExpensesXML(reports));
+            // Generating monthly report by clinic as XML file...            
+
+            // AT THE MOMENT WORKS WITH FAKE DATA, UNCOMMENT THE METHOD BELOW TO WORK WITH THE DATABASE
+            //var reports = RetrieveReportFromDB(year, month);
+            var reports = RetrieveReportTest(year, month);
+            var clinicsReport = new XElement("reports", GetExpensesXML(reports));
             clinicsReport.Save("../../Reports/Clinics-Monhtly-Reports.xml");
 
             MessageBox.Show("XML report generated in the Reports folder.");
@@ -33,7 +36,7 @@
             foreach (var report in reports)
             {
                 var dailySummaries = GetDailySummariesXML(report.Value);
-                var expense = new XElement("expense", dailySummaries);
+                var expense = new XElement("report", dailySummaries);
                 expense.SetAttributeValue("clinic", report.Key);
                 sales.Add(expense);
             }
@@ -47,7 +50,7 @@
             foreach (var item in report)
             {
                 var elementAsString = String.Format(
-                    @"<summary date=""{0}"" total-sum=""{1:.00}"" />",
+                    @"<summary date=""{0}"" expense=""{1:.00}"" />",
                     item.Key.ToShortDateString(),
                     item.Value);
 
@@ -66,17 +69,19 @@
             {
                 var result = db.Manipulations.Where(m => m.Date.Year == year && m.Date.Month == month);
 
+                // This is how it will look:
+
                 //    <?xml version="1.0" encoding="utf-8"?>
-                //    <expenses>
-                //      <expense clinic="Clinic A">
-                //        <expenses month="Jul-2013">30.00</expenses>
-                //        <expenses month="Aug-2013">40.00</expenses>
-                //      </expense>
-                //      <expense clinic="Clinic B">
-                //        <expenses month="Jul-2013">200.00</expenses>
-                //        <expenses month="Aug-2013">180.00</expenses>
-                //      </expense>
-                //    </expenses>
+                //    <reports>
+                //      <report clinic="Clinic A">
+                //        <summary date="8/29/2014" expense="12365.01" />
+                //        <summary date="8/19/2014" expense="4545.20" />
+                //      </report>
+                //      <report clinic="Clinic B">
+                //        <summary date="9/11/2014" expense="12365.01" />
+                //        <summary date="8/22/2014" expense="4545.20" />
+                //      </report>
+                //    </reports>
 
                 foreach (var manipulation in db.Manipulations)
                 {
@@ -101,6 +106,22 @@
 
                 return dailyByClinic;
             }
+        }
+
+        public static Dictionary<string, Dictionary<DateTime, Decimal>> RetrieveReportTest(int year, int month)
+        {
+            var dailyByClinic = new Dictionary<string, Dictionary<DateTime, decimal>>();
+
+            dailyByClinic.Add("Clinic A", new Dictionary<DateTime, decimal>());
+            dailyByClinic.Add("Clinic B", new Dictionary<DateTime, decimal>());
+
+            dailyByClinic["Clinic A"].Add(DateTime.Now, 12365.01m);
+            dailyByClinic["Clinic A"].Add(DateTime.Now.AddDays(-10), 4545.20m);
+
+            dailyByClinic["Clinic B"].Add(DateTime.Now.AddDays(13), 12365.01m);
+            dailyByClinic["Clinic B"].Add(DateTime.Now.AddDays(-7), 4545.20m);
+
+            return dailyByClinic;
         }
     }
 }
