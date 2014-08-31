e042224a9f23b4c6d7ac47a852f8d2f9c954097d
neutrino <jrusev@gmail.com>
2014-08-31 15:32:32 +0300

    added SQLite db with data and method to read it

--- Clinics/Clinics/Exports/ExportToExcel.cs
+++ Clinics/Clinics/Exports/ExportToExcel.cs
@@ -4,8 +4,6 @@
     using System;
     using System.Windows.Forms;
     using System.Configuration;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Data.SQLite;
     using System.Data;
 
