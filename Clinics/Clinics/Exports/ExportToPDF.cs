0e95344a4691ecb40e656cdb0493b58b2130df00
nkichev <nkichev@abv.bg>
2014-09-01 12:33:57 +0300

    PDF reporting code tuning

--- Clinics/Clinics/Exports/ExportToPDF.cs
+++ Clinics/Clinics/Exports/ExportToPDF.cs
@@ -3,12 +3,12 @@
     using System;
     using System.IO;
     using System.Linq;
+    using System.Text;
     using System.Windows.Forms;
 
     using Clinics.Data;
     using iTextSharp.text;
     using iTextSharp.text.pdf;
-    using System.Text;
 
     public partial class ExportToPDF : Form
     {
@@ -22,45 +22,54 @@
             int month = this.Month.SelectedIndex + 1;
             int year = int.Parse(this.Year.Text);
 
-            IClinicsData db = new ClinicsData();
-            var dbManipulations = db.Manipulations.All().Where(m => m.Date.Year == year && m.Date.Month == month);
-            var dbProcedures = db.Procedures.All().ToList();
-            var dbPatients = db.Patients.All().ToList();
-            var dbSpecialists = db.Specialists.All().ToList();
-            var dbTitles = db.Titles.All().ToList();
-
             Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
             PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("../../Reports/test.pdf", FileMode.Create));
             doc.Open();
 
-            //Create title header
+            this.CreateTitleHeader(doc);
+
+            this.CreateTableHeader(doc, month, year);
+
+            this.CreateTable(doc, month, year);
+
+            doc.Close();
+
+            MessageBox.Show("PDF report generated in the Reports folder.");
+        }
+
+        private void CreateTitleHeader(Document doc)
+        {
             PdfPTable titleHeader = new PdfPTable(1);
 
             
-            PdfPCell cellHeader = new PdfPCell(MakeBoldPhrase("Aggregated Procedures Report"));
+            PdfPCell cellHeader = new PdfPCell(this.MakeBoldPhrase("Aggregated Procedures Report"));
             cellHeader.HorizontalAlignment = 1;
 
             titleHeader.AddCell(cellHeader);
 
-            //Create table header
+            doc.Add(titleHeader);
+        }
+
+        private void CreateTableHeader(Document doc, int month, int year)
+        {
             PdfPTable tableHeader = new PdfPTable(5);
             PdfPCell cellHeaderDate = new PdfPCell(new Phrase("Date: " + month + "-" + year));
             cellHeaderDate.BackgroundColor = new BaseColor(217, 217, 217);
             cellHeaderDate.Colspan = 5;
 
-            PdfPCell procedure = new PdfPCell(MakeBoldPhrase("Procedure"));
+            PdfPCell procedure = new PdfPCell(this.MakeBoldPhrase("Procedure"));
             procedure.BackgroundColor = new BaseColor(217, 217, 217);
 
-            PdfPCell manipulation = new PdfPCell(MakeBoldPhrase("Manipulation"));
+            PdfPCell manipulation = new PdfPCell(this.MakeBoldPhrase("Manipulation"));
             manipulation.BackgroundColor = new BaseColor(217, 217, 217);
 
-            PdfPCell date = new PdfPCell(MakeBoldPhrase("Date"));
+            PdfPCell date = new PdfPCell(this.MakeBoldPhrase("Date"));
             date.BackgroundColor = new BaseColor(217, 217, 217);
 
-            PdfPCell patient = new PdfPCell(MakeBoldPhrase("Patient"));
+            PdfPCell patient = new PdfPCell(this.MakeBoldPhrase("Patient"));
             patient.BackgroundColor = new BaseColor(217, 217, 217);
 
-            PdfPCell specialist = new PdfPCell(MakeBoldPhrase("Specialist"));
+            PdfPCell specialist = new PdfPCell(this.MakeBoldPhrase("Specialist"));
             specialist.BackgroundColor = new BaseColor(217, 217, 217);
 
             tableHeader.AddCell(cellHeaderDate);
@@ -70,7 +79,18 @@
             tableHeader.AddCell(patient);
             tableHeader.AddCell(specialist);
 
-            //Create table body
+            doc.Add(tableHeader);
+        }
+
+        private void CreateTable(Document doc, int month, int year)
+        {
+            IClinicsData db = new ClinicsData();
+            var dbManipulations = db.Manipulations.All().Where(m => m.Date.Year == year && m.Date.Month == month);
+            var dbProcedures = db.Procedures.All().ToList();
+            var dbPatients = db.Patients.All().ToList();
+            var dbSpecialists = db.Specialists.All().ToList();
+            var dbTitles = db.Titles.All().ToList();
+
             PdfPTable tableBody = new PdfPTable(5);
 
             foreach (var man in dbManipulations)
@@ -109,15 +129,8 @@
             TotalManipulationsCell.HorizontalAlignment = 1;
             footer.AddCell(TotalManipulationsCell);
 
-            //Add to document
-            doc.Add(titleHeader);
-            doc.Add(tableHeader);
             doc.Add(tableBody);
             doc.Add(footer);
-
-            doc.Close();
-
-            MessageBox.Show("PDF report generated in the Reports folder.");
         }
 
         private Phrase MakeBoldPhrase(string text) 
