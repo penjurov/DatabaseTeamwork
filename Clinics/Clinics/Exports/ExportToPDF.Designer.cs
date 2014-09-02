bc8aabd4fd0deb3419512d732cf43cef33f1fff4
Rosica Boykova <rossi_308@abv.bg>
2014-09-02 12:57:58 +0300

    Some design changes added

--- Clinics/Clinics/Exports/ExportToPDF.Designer.cs
+++ Clinics/Clinics/Exports/ExportToPDF.Designer.cs
@@ -34,6 +34,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExportToPDF));
             this.button1 = new System.Windows.Forms.Button();
             this.Month = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -43,15 +44,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(242, 22);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(363, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Export";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.ExportToPDF_click);
             // 
-            // comboBox1
+            // Month
             // 
             this.Month.FormattingEnabled = true;
             this.Month.Items.AddRange(new object[] {
@@ -67,46 +70,55 @@
             "October",
             "November",
             "December"});
-            this.Month.Location = new System.Drawing.Point(12, 24);
-            this.Month.Name = "comboBox1";
-            this.Month.Size = new System.Drawing.Size(118, 21);
+            this.Month.Location = new System.Drawing.Point(18, 30);
+            this.Month.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(175, 24);
             this.Month.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.Size = new System.Drawing.Size(54, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Month";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(135, 8);
+            this.label2.Location = new System.Drawing.Point(202, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.Size = new System.Drawing.Size(39, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Year";
             // 
-            // textBox1
+            // Year
             // 
-            this.Year.Location = new System.Drawing.Point(136, 24);
-            this.Year.Name = "textBox1";
-            this.Year.Size = new System.Drawing.Size(100, 20);
+            this.Year.Location = new System.Drawing.Point(204, 30);
+            this.Year.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(148, 23);
             this.Year.TabIndex = 4;
             // 
             // ExportToPDF
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(686, 385);
             this.Controls.Add(this.Year);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Month);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ExportToPDF";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Export To PDF";
--- Clinics/Clinics/Imports/ImportFromMongoDB.Designer.cs
+++ Clinics/Clinics/Imports/ImportFromMongoDB.Designer.cs
@@ -30,25 +30,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportFromMongoDB));
             this.import = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // import
             // 
-            this.import.Location = new System.Drawing.Point(12, 12);
+            this.import.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.import.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("import.BackgroundImage")));
+            this.import.Location = new System.Drawing.Point(13, 13);
+            this.import.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.import.Name = "import";
-            this.import.Size = new System.Drawing.Size(121, 23);
+            this.import.Size = new System.Drawing.Size(182, 32);
             this.import.TabIndex = 1;
             this.import.Text = "Import from Mongo";
-            this.import.UseVisualStyleBackColor = true;
+            this.import.UseVisualStyleBackColor = false;
             this.import.Click += new System.EventHandler(this.ImportFromMongo_Click);
             // 
             // ImportFromMongoDB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(686, 385);
             this.Controls.Add(this.import);
+            this.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ImportFromMongoDB";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Import from MongoDB";
